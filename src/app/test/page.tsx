14ff6cc675083fdf2a2989cc64871e792ded6ec8
Rafsan2507 <khanrafsan25@gmail.com>
2025-08-25 09:50:04 +0600

    added file

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,7 +1,26 @@
-"use client";
-import PersistentDrawerLeft from "@/components/muiNavComponent/drawer";
+// export const dynamic = "force-dynamic";
+// import PersistentDrawerLeft from "@/components/muiNavComponent/Drawer";
+// //import PersistentDrawerLeft from "@/components/muiNavComponent/Drawer";
+// import React from "react";
 
+// function Page() {
+//   return (
+//     <div>
+//       <PersistentDrawerLeft />
+//     </div>
+//   );
+// }
+
+// export default Page;
+export const dynamic = "force-dynamic";
 import React from "react";
+import nextDynamic from "next/dynamic";
+
+// disable SSR for this component
+const PersistentDrawerLeft = nextDynamic(
+  () => import("@/components/muiNavComponent/Drawer"),
+  { ssr: false }
+);
 
 function Page() {
   return (
